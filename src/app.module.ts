d6333115b09993880cf55078ca9d860f68fac6b8
MDPereyra22 <pereyra.dmatias@gmail.com>
2024-05-18 10:27:44 -0300

    Crea estructura de proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
+import { AuthorsModule } from './authors/authors.module';
+import { EditorialsModule } from './editorials/editorials.module';
+
 
 @Module({
-  imports: [],
+  imports: [ BooksModule, AuthorsModule, EditorialsModule],
   controllers: [AppController],
   providers: [AppService],
 })
