6032637539b49ae272ddeb75ba8e2e177d4fd1dc
MDPereyra22 <pereyra.dmatias@gmail.com>
2024-05-18 13:31:09 -0300

    actualiza controller de editoriales

--- src/editoriales/editoriales.controller.ts
+++ src/editoriales/editoriales.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { Editorial } from './entities/editorial.entity';
+import { EditorialesService } from './editoriales.service';
 
 @Controller('editoriales')
-export class EditorialesController {}
+export class EditorialesController {
+    constructor(
+        private readonly editorialesService: EditorialesService
+    ) { }
+
+    @Get()
+    findAll():Promise<Editorial[]>{
+        return this.editorialesService.findAll()
+    }
+    @Get(':id')
+    findOne(@Param('id') id: number) : Promise<Editorial>{
+        return this.editorialesService.findOne(id)
+    }
+
+    @Post()
+    create(@Body() editorial: Editorial) : Promise<Editorial>{
+        return this.editorialesService.createEditorial(editorial)
+    }
+
+    @Put(':id')
+    update(@Param('id') id: number, @Body() editorial: Editorial) : Promise<Editorial>{
+        return this.editorialesService.updateEditorial(id,editorial)
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id:number) : Promise<void>{
+        return this.editorialesService.removeEditorial(id)
+    }
+}
