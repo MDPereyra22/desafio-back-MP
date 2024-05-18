82fb85bd26c5e162c7e6157511492d5086288992
MDPereyra22 <pereyra.dmatias@gmail.com>
2024-05-18 13:24:57 -0300

    Actualiza controller de autores

--- src/autores/autores.controller.ts
+++ src/autores/autores.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { Autor } from './entities/autor.entity';
+import { AutoresService } from './autores.service';
 
-@Controller('autores')
-export class AutoresController {}
+@Controller('autoes')
+export class AutoressController {
+    constructor(
+        private readonly autoresService: AutoresService
+    ) { }
+
+    @Get()
+    findAll():Promise<Autor[]>{
+        return this.autoresService.findAll()
+    }
+    @Get(':id')
+    findOne(@Param('id') id: number) : Promise<Autor>{
+        return this.autoresService.findOne(id)
+    }
+
+    @Post()
+    create(@Body() autor: Autor) : Promise<Autor>{
+        return this.autoresService.createAutor(autor)
+    }
+
+    @Put(':id')
+    update(@Param('id') id: number, @Body() autor: Autor) : Promise<Autor>{
+        return this.autoresService.updateAutor(id,autor)
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id:number) : Promise<void>{
+        return this.autoresService.remove(id)
+    }
+}
