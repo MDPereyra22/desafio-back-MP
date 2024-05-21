71c8b6a2445adf2abf4ae270cf0c1a562dbd4b2e
MDPereyra22 <pereyra.dmatias@gmail.com>
2024-05-21 15:17:21 -0300

    Actualiza controlador de autores para uso de dto

--- src/autores/autores.controller.ts
+++ src/autores/autores.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
 import { Autor } from './entities/autor.entity';
 import { AutoresService } from './autores.service';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { CreateAutorDto } from './dto/create-autor.dto';
+import { UpdateAutorDto } from './dto/update-autor.dto';
 
 @Controller('autores')
 export class AutoresController {
@@ -22,14 +24,19 @@ export class AutoresController {
 
     @Post()
     @ApiOperation({ summary: 'Crear un nuevo autor' })
-    create(@Body() autor: Autor) : Promise<Autor>{
-        return this.autoresService.createAutor(autor)
+    @ApiResponse({ status: 201, description: 'El autor ha sido creado con éxito.', type: Autor })
+    @ApiResponse({ status: 400, description: 'Datos de entrada incorrectos.' })
+    async create(@Body() createAutorDto: CreateAutorDto): Promise<Autor> {
+      return this.autoresService.createAutor(createAutorDto);
     }
 
     @Put(':id')
-    @ApiOperation({ summary: 'Actualizar un autor existentes' })
-    update(@Param('id') id: number, @Body() autor: Autor) : Promise<Autor>{
-        return this.autoresService.updateAutor(id,autor)
+    @ApiOperation({ summary: 'Actualizar un autor existente' })
+    @ApiResponse({ status: 200, description: 'El autor ha sido actualizado con éxito.', type: Autor })
+    @ApiResponse({ status: 404, description: 'El autor con el ID especificado no existe.' })
+    @ApiResponse({ status: 400, description: 'Datos de entrada incorrectos.' })
+    async update(@Param('id') id: number, @Body() updateAutorDto: UpdateAutorDto): Promise<Autor> {
+      return this.autoresService.updateAutor(id, updateAutorDto);
     }
 
     @Delete(':id')
